f2ec370af64e426c5260dba04b6bec26b74ec019
harishm8667 <111110844+harishm8667@users.noreply.github.com>
2022-08-19 10:48:02 +0530

    Removing code that doesn’t require since every unauthorized request must be ended. (#3)
    * code doesn’t require since every unauthorized request must be ended
    
    * code doesn’t require since every unauthorized request must be ended

--- app/redfish-handler.lua
+++ app/redfish-handler.lua
@@ -501,10 +501,7 @@ end
 --- Inherited modules can use this function to mark the session as unauthorized
 -- @param msg optional Sets an optional message
 function RedfishHandler:unauthorized(msg)
-	if finished then
-		return
-	end
-
+	
 	self:add_header("WWW-Authenticate", 'Basic realm="AMI_Redfish_Server"') --TODO make the realm unique
 	self:add_header("WWW-Authenticate", 'X-Auth-Token realm="AMI_Redfish_Server"') --TODO make the realm unique
 
